f4a5ea6e33792853f5a3ce6507276a70af8f6748
haruna-yoshida <hattan.nattsun@icloud.com>
2021-09-17 10:32:56 +0000

    update_main.py

--- main.py
+++ main.py
@@ -8,7 +8,34 @@ from statistics import mode
 import csv
 import pprint
 
-def get_concept_number(quest:str) -> list:
+def get_node_info(quest:str) -> (list,list):
+    """
+    mecabを用いて引数用のnodeを生成する
+
+    Parameters
+    ----------
+    quest : str
+        問題文
+    
+    Returns
+    -------
+    node_features : list
+    node_surfaces : list
+        node（形態素解析されたもの）を返す
+    """
+
+    m = MeCab.Tagger("-Ochasen")
+    node = m.parseToNode(quest)
+    node_features = []
+    node_surfaces = []
+    while node:
+        node_features.append(node.feature)
+        node_surfaces.append(node.surface)
+        node=node.next
+    return node_features[1:-1], node_surfaces[1:-1]
+
+
+def get_concept_number(quest:str,node_features:list,node_surfaces:list):
     """
     正規化処理のために概念と数量のlistを取得する。
 
@@ -22,45 +49,47 @@ def get_concept_number(quest:str) -> list:
     concept_number_tuple : list
         序数詞に1番近い一般名詞tuple型にしてlistに格納
     """
-
-
-    m = MeCab.Tagger("-Ochasen")
-    node = m.parseToNode(quest)
+    
     words=[]
     nums=[]
     verbs = []
     concept_number_tuple = []
-
+    
     i = 1
 
-    while node:
-        hinshi = node.feature.split(",")[1]
+    for node_feature, node_surface in zip(node_features,node_surfaces):
+        hinshi = node_feature.split(",")[1]
         if hinshi in ["一般"]:
-            # print(node.surface)
-            origin = node.feature.split(",")[6]
+            # print(node_surface)
+            origin = node_feature.split(",")[6]
             words.append((origin,i))
+            
 
         if hinshi in ["数"]:
-            # print(node.surface.split(","))
-            origin = node.surface.split(",")[0]
+            # print(node_surface.split(","))
+            origin = node_surface.split(",")[0]
             nums.append((origin,i))
         
 
-        if node.feature.split(",")[0] in ["動詞","助動詞"] :
-            # print(node.surface.split(","))
-            origin = node.surface.split(",")[0]
+        if node_feature.split(",")[0] in ["動詞","助動詞"] :
+            # print(node_surface.split(","))
+            origin = node_surface.split(",")[0]
             verbs.append((origin,i))
-        node = node.next
+        
+        
         
         i += 1
 
+
     for num in nums:
         min_string = words[0][0]
+        min_index = words[0][1]
         min_diff = 9999
         for word in words:
             diff = abs(word[1] - num[1])
             if diff < min_diff:
                 min_string = word[0]
+                min_index = word[1]
                 min_diff = diff
             
             min_verbs = verbs[0][0]
@@ -74,11 +103,10 @@ def get_concept_number(quest:str) -> list:
         # min_stringとnumのtuple型をlistに格納
         # concept_number_tuple.append((min_string,num[0]))
         
-        concept_number_tuple.append((min_string,num[0],min_verbs))
+        concept_number_tuple.append((min_string,num[0],min_verbs,min_index))
 
 
     return concept_number_tuple
-        
     
 def get_standard_list(normalize_list:list) -> list:
     """
@@ -110,17 +138,72 @@ def get_standard_list(normalize_list:list) -> list:
     #allduplicate_listの再頻出単語
     base_word = (mode(allduplicate_list))
     for normalize_word in normalize_list:
-        standard_list.append((base_word,normalize_word[1],normalize_word[2]))
+        standard_list.append((base_word,normalize_word[1],normalize_word[2],normalize_word[3]))
     return standard_list
+
+def get_normalize_table(quest:str,node_features:list,node_surfaces:list):
+    #変化明示語からbefore,increaseなどに分ける
+    m = MeCab.Tagger("-Ochasen")
+    node = m.parseToNode(quest)
+    important_words = []
+    keywords = []
+    concept_number_tuple = []
+    table = []
+
+    i = 1
+    
+    for node_feature, node_surface in zip(node_features,node_surfaces):
+        keywords = node_surface.split(",")
+        for time_expression_word in time_expression_words:
+            for keyword in keywords:
+                #print(keyword)
+                # print(time_expression_word[0])
+                if time_expression_word[0] == keyword:
+                    important_words.append((keyword,i,time_expression_word[1]))
+        i += 1
+                
+    # print(important_words)
+            # print(node_surface)
+            # origin = node_feature.split(",")[6]
+            # keywords.append((keyword,i))
+            # print(keywords)
+                
+    for important_word in important_words:
+        min_element = standard_list[0]
+        # print(standard_list)
+        min_diff = 9999
+        for element in standard_list:
+            diff = abs(element[3] - important_word[1])
+            print("{0}:{2} - {1}{3}  = {4}".format(element[0],important_word[0],element[3],important_word[1],diff))
+            if diff < min_diff:
+                min_element = element
+                min_diff = diff
+        table.append({important_word[2]:min_element}) 
+        print(table)
+            
+        
+        
+        
+
 
 if __name__ == "__main__":
     with open('/workspace/NLP-Container/data/sample_questions.csv' , encoding ="utf_8") as f:
         reader = csv.reader(f)
         quest_list = [row for row in reader]
-    quest = quest_list[2][4]
-    normalize_list = get_concept_number(quest)
-    print(normalize_list)
+    quest = quest_list[14][4]
+    print(quest)
+    with open('/workspace/NLP-Container/data/time_expression_base.csv', encoding ="utf_8") as f:
+        reader = csv.reader(f)
+        time_expression_word_list = [row for row in reader]
+    time_expression_words = time_expression_word_list[1:]
+    # print(time_expression_words)
+    node_features,node_surfaces = get_node_info(quest)
+    normalize_list = get_concept_number(quest,node_features,node_surfaces)
     print("正規化処理完了")
+    print(normalize_list)
     standard_list = get_standard_list(normalize_list)
     print("共通概念処理完了")
     print(standard_list)
+    normalize_table = get_normalize_table(quest,node_features,node_surfaces)
+    print(get_normalize_table)
+    
--- update_csv_data.sh
+++ update_csv_data.sh
@@ -4,3 +4,5 @@ wget --no-check-certificate "https://docs.google.com/spreadsheets/d/e/2PACX-1vTP
 wget --no-check-certificate "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPJ18E6_Y6LL7SnujvwbvfJthfiKjKRrzSXz548xvk9cuiE76qq-MKIYhzv51Y4OuMHrP1pxbWgHIc/pub?gid=1805324838&single=true&output=csv" -O data/unit_knowledge_base.csv
 # quest split base
 wget --no-check-certificate "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPJ18E6_Y6LL7SnujvwbvfJthfiKjKRrzSXz548xvk9cuiE76qq-MKIYhzv51Y4OuMHrP1pxbWgHIc/pub?gid=1524513757&single=true&output=csv" -O data/quest_split_base.csv
+# time expression base
+wget --no-check-certificate "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPJ18E6_Y6LL7SnujvwbvfJthfiKjKRrzSXz548xvk9cuiE76qq-MKIYhzv51Y4OuMHrP1pxbWgHIc/pub?gid=272033425&single=true&output=csv"-O data/time_expression_base.csv
